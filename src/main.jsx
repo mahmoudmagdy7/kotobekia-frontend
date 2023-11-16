97911e91974392dd7edd4a4920b9e33d0f541aba
mahmoudmagdy7 <mahmoudamgdy.r7@gmail.com>
2023-11-17 00:50:33 +0200

    -Add the main routing -Add tailwind and themes of , add solar icon -Create the project

---
+++ src/main.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./pages/Home.jsx";
+import "./index.css";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider } from "@material-tailwind/react";
+import Layout from "./pages/Layout.jsx";
+const route = createBrowserRouter([
+  // { path: "/", element: <Home /> },
+  { path: "*", element: <h1>Not found</h1> },
+  // { path: "not-authorized", element: <NoAccess /> },
+  { path: "/auth/login", element: <h1>Login</h1> },
+  { path: "/auth/register", element: <h1>Register</h1> },
+  { path: "/auth/banned", element: <h1>Banned</h1> },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/category", element: <h1>category</h1> },
+      { path: "/ad", element: <h1>ad page</h1> },
+      { path: "/ad/new", element: <h1>Adding new add</h1> },
+      { path: "/account/:id", element: <h1>User account</h1> },
+      {
+        path: "/chat",
+        element: (
+          <h1>
+            User Chats <Outlet />
+          </h1>
+        ),
+        children: [{ path: ":chatId", element: <h1>user chat</h1> }],
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <NextUIProvider>
+    <ThemeProvider>
+      <RouterProvider router={route} />
+    </ThemeProvider>
+  </NextUIProvider>
+);
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+// tailwind.config.js
+const { nextui } = require("@nextui-org/react");
+const withMT = require("@material-tailwind/react/utils/withMT");
+/** @type {import('tailwindcss').Config} */
+module.exports = withMT({
+  content: [
+    // ...
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+});
