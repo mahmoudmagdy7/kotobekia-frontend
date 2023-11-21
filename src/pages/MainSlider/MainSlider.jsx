3e7cecc04f142cafb77f52be5d23585cb8dfa0bc
Mohamed Elbana <88998148+Mo7amed-2lbana@users.noreply.github.com>
2023-11-21 16:59:23 +0200

    Create Navbar Category and Slider

---
+++ src/pages/MainSlider/MainSlider.jsx
@@ -0,0 +1,12 @@
+import slider from "../../assets/slider.png";
+
+const MainSlider = () => {
+  return (
+    <>
+      <section className="slider">
+        <img src={slider} alt="Slider" />
+      </section>
+    </>
+  );
+};
+export default MainSlider;
