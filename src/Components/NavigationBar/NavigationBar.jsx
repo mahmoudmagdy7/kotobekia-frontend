5d9ebd924c832c99b9b58c6241b1d5a59c1f5c04
mahmoudmagdy7 <mahmoudamgdy.r7@gmail.com>
2023-11-27 02:48:02 +0200

    Added Bottom navigation bar to the components

---
+++ src/Components/NavigationBar/NavigationBar.jsx
@@ -0,0 +1,36 @@
+import { Button } from "@nextui-org/react";
+import { useTranslation } from "react-i18next";
+import * as solarIcons from "solar-icon-set";
+
+function NavigationBar() {
+  const { t } = useTranslation();
+  return (
+    <div>
+      <ul className="flex items-center justify-around lg:hidden gap-1 fixed start-0 end-0 bg-white bottom-0 pb-1 pt-2 rounded-t-3xl  sm:text-sm px-3">
+        <li className="flex items-center flex-col">
+          <solarIcons.User size={23} color="#28D8AE" />
+          <span className="font-bold">{t("navigation_bar.account")}</span>{" "}
+        </li>{" "}
+        <li className="flex items-center flex-col">
+          <solarIcons.Widget4 size={23} color="#28D8AE" />
+          <span className="font-bold">{t("navigation_bar.my_posts")}</span>
+        </li>{" "}
+        <li className="flex items-center flex-col bottom-4 relative home-icon">
+          <Button isIconOnly variant="flat" color="transparent" className="p-0">
+            <solarIcons.AddSquare size={35} color="#28D8AE" />
+          </Button>{" "}
+        </li>{" "}
+        <li className="flex items-center flex-col">
+          <solarIcons.Letter size={23} color="#28D8AE" />
+          <span className="font-bold">{t("navigation_bar.messages")}</span>
+        </li>{" "}
+        <li className="flex items-center flex-col">
+          <solarIcons.Home size={23} color="#28D8AE" />
+          <span className="font-bold">{t("navigation_bar.home")}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default NavigationBar;
--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -1,12 +1,13 @@
 import { Button } from "@nextui-org/react";
 import Cookies from "js-cookie";
 import { useTranslation } from "react-i18next";
+import NavigationBar from "../../Components/NavigationBar/NavigationBar";
 
 function Category() {
   const { t } = useTranslation();
 
   return (
-    <div className="text-red-500">
+    <div className="text-black">
       {t("governorates.cairo")}
 
       <Button
@@ -24,6 +25,23 @@ function Category() {
       >
         click me!
       </Button>
+
+      {/* <Button
+        onClick={async () => {
+          if (localStorage.getItem("i18nextLng") == "en") {
+            localStorage.setItem("i18nextLng", "ar");
+            window.location.reload();
+            document.body.dir = "rtl";
+          } else {
+            localStorage.setItem("i18nextLng", "en");
+            window.location.reload();
+            document.body.dir = "ltr";
+          }
+        }}
+      >
+        click me!
+      </Button> */}
+      <NavigationBar />
     </div>
   );
 }
