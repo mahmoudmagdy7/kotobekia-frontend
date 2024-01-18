3d5c0a7095f98ada942834c54f8ccb5b44174142
mahmoudmagdy7 <mahmoudamgdy.r7@gmail.com>
2024-01-18 06:52:10 +0200

    Category filtering added , Close #41

--- src/pages/Category/MobileFilters.jsx
+++ src/pages/Category/MobileFilters.jsx
@@ -5,12 +5,30 @@ import * as solarIcons from "solar-icon-set";
 import { Accordion, AccordionBody } from "@material-tailwind/react";
 import { CheckboxGroup, Checkbox } from "@nextui-org/react";
 
-import { useState } from "react";
-function DesktopFilters() {
+import { useEffect, useState } from "react";
+function DesktopFilters({ updateFilters }) {
   const { t } = useTranslation();
 
   const handleOpenAcc1 = () => setOpenAcc1((cur) => !cur);
   const [openAcc1, setOpenAcc1] = useState(false);
+  const [filtersOptions, setFiltersOptions] = useState([]);
+
+  function getFilterValue(value) {
+    if (filtersOptions.includes(value)) {
+      const idx = filtersOptions.indexOf(value);
+      filtersOptions.splice(idx, 1);
+      setFiltersOptions((prev) => [...prev]);
+    } else {
+      setFiltersOptions((prev) => [...prev, value]);
+    }
+  }
+  useEffect(() => {
+    let toString = "";
+    filtersOptions.forEach((item) => {
+      toString += item;
+    });
+    updateFilters(toString);
+  }, [filtersOptions]);
   return (
     <div className="block lg:hidden">
       <Accordion open={openAcc1}>
@@ -20,7 +38,7 @@ function DesktopFilters() {
         </Button>
         <AccordionBody>
           <div>
-            {/* ============================== Education Level ============================== */}
+            {/* ============================== Education Level ==============================
             <CheckboxGroup color="success">
               <h3 className="font-semibold text-lg">{t("education_levels.title")}</h3>
               <div className="flex flex-wrap gap-3">
@@ -40,27 +58,39 @@ function DesktopFilters() {
                   {t("education_levels.general")}
                 </Checkbox>
               </div>
+            </CheckboxGroup> */}
+            {/* ============================== price Type ============================== */}
+            <CheckboxGroup color="success" className="mt-5">
+              <h3 className="font-semibold text-lg">price</h3>
+              <div className="flex flex-wrap gap-3">
+                <Checkbox onClick={() => getFilterValue("&price=0")} className="check-button" value={0}>
+                  free
+                </Checkbox>{" "}
+                <Checkbox onClick={() => getFilterValue("&price=1")} className="check-button" value={1}>
+                  paid
+                </Checkbox>
+              </div>
             </CheckboxGroup>
             {/* ============================== Grade ============================== */}
             <CheckboxGroup color="success" className="mt-5">
               <h3 className="font-semibold text-lg">{t("grades.title")}</h3>
               <div className="flex flex-wrap gap-3">
-                <Checkbox className="check-button" value="grade_one">
+                <Checkbox onClick={() => getFilterValue("&grade=grade_one")} className="check-button" value="grade_one">
                   {t("grades.grade_one")}
                 </Checkbox>
-                <Checkbox className="check-button" value="grade_two">
+                <Checkbox onClick={() => getFilterValue("&grade=grade_two")} className="check-button" value="grade_two">
                   {t("grades.grade_two")}
                 </Checkbox>
-                <Checkbox className="check-button" value="grade_three">
+                <Checkbox onClick={() => getFilterValue("&grade=grade_three")} className="check-button" value="grade_three">
                   {t("grades.grade_three")}
                 </Checkbox>
-                <Checkbox className="check-button" value="grade_four">
+                <Checkbox onClick={() => getFilterValue("&grade=grade_four")} className="check-button" value="grade_four">
                   {t("grades.grade_four")}
                 </Checkbox>
-                <Checkbox className="check-button" value="grade_five">
+                <Checkbox onClick={() => getFilterValue("&grade=grade_five")} className="check-button" value="grade_five">
                   {t("grades.grade_five")}
                 </Checkbox>
-                <Checkbox className="check-button" value="KG">
+                <Checkbox onClick={() => getFilterValue("&grade=grade_six")} className="check-button" value="KG">
                   {t("grades.grade_six")}
                 </Checkbox>
               </div>
@@ -69,14 +99,14 @@ function DesktopFilters() {
             <CheckboxGroup color="success" className="mt-5">
               <h3 className="font-semibold text-lg">{t("education_type.title")}</h3>
               <div className="flex flex-wrap gap-3">
-                <Checkbox className="check-button" value="general">
+                <Checkbox onClick={() => getFilterValue("&educationType=general")} className="check-button" value="general">
                   {t("education_type.general")}
                 </Checkbox>{" "}
-                <Checkbox className="check-button" value="azhar">
+                <Checkbox onClick={() => getFilterValue("&educationType=azhar")} className="check-button" value="azhar">
                   {t("education_type.azhar")}
                 </Checkbox>
               </div>
-            </CheckboxGroup>
+            </CheckboxGroup>{" "}
           </div>{" "}
         </AccordionBody>
       </Accordion>
